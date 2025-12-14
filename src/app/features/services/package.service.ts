d9ce7d9ed968bf560b8fe9e26f670217d4509f1e
Muhammed Ötün <beyfendipenguen@gmail.com>
2025-12-14 14:11:38 +0300

    refactor: remove unnecessary import

--- src/app/features/services/package.service.ts
+++ src/app/features/services/package.service.ts
@@ -2,7 +2,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { GenericCrudService } from "@app/core/services/generic-crud.service";
-import { PackageDetailWriteDto } from "../interfaces/package-detail.interface";
 import { PackageReadDto } from "../interfaces/package.interface";
 
 @Injectable({
