536f51d1d5b9f2bbf2901e676d8f8fde5f876d37
hllyrtmn <halilyurtman@gmail.com>
2025-12-27 15:33:15 +0300

    updated

--- deploy.php
+++ deploy.php
@@ -1,19 +1,30 @@
 <?php
 $secret_key = 'qzeoFLq6SSo7Dah8Y3C';
 
-if ($_GET['key'] != $secret_key) {
-    die('Access denied.');
+if (!isset($_GET['key']) || $_GET['key'] != $secret_key) {
+    die('Erişim reddedildi.');
 }
 
-$cpanel_user = 'ind16aricodecom';
+// AYARLAR
+// 1. Repo yolunu kontrol et (cPaneldekinin aynısı olmalı)
 $repo_path = '/home/ind16aricodecom/repositories/binpacking_angular_test';
 
-error_log("Deploy triggered: " . date("Y-m-d H:i:s"));
+// 2. Hangi branch deploy edilecek? (Burası "staging" olmalı)
+$branch_name = 'staging';
 
-$command = "uapi VersionControl update repository_root=$repo_path branch=staging source_repository='{\"remote_name\":\"origin\"}' 2>&1";
+// LOGLAMA BAŞLANGICI
+error_log("Deploy tetiklendi. Hedef Branch: " . $branch_name);
 
+// KOMUTU HAZIRLA
+$command = "uapi VersionControl update repository_root=$repo_path branch=$branch_name source_repository='{\"remote_name\":\"origin\"}' 2>&1";
+
+// ÇALIŞTIR
 exec($command, $output);
 
+// LOGLAMA SONUCU (Burası cevabı error_log'a yazacak)
+error_log("Deploy Sonucu: " . print_r($output, true));
+
+// Ekrana da bas (Manuel tetiklemeler için)
 echo "<pre>";
 print_r($output);
 echo "</pre>";
