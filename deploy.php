d5cd73a2be142bdcec8fb780d83b52c52a1cba88
hllyrtmn <halilyurtman@gmail.com>
2025-12-27 14:30:58 +0300

    updated

---
+++ deploy.php
@@ -0,0 +1,20 @@
+<?php
+$secret_key = 'qzeoFLq6SSo7Dah8Y3C';
+
+if ($_GET['key'] != $secret_key) {
+    die('Access denied.');
+}
+
+$cpanel_user = 'ind16aricodecom';
+$repo_path = '/home/ind16aricodecom/repositories/binpacking_angular_test';
+
+error_log("Deploy triggered: " . date("Y-m-d H:i:s"));
+
+$command = "uapi VersionControl update repository_root=$repo_path branch=stepper-refactor source_repository='{\"remote_name\":\"origin\"}' 2>&1";
+
+exec($command, $output);
+
+echo "<pre>";
+print_r($output);
+echo "</pre>";
+?>
