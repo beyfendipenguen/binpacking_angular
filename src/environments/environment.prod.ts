2291a352c6405dbee1db53850db9b24c66d65404
hllyrtmn <halilyurtman@gmail.com>
2025-12-27 13:55:07 +0300

    preprod release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: true,
     name: 'deploy',
     apiUrl: 'https://api.industricode.com/api',
-    appVersion: '1766706330419'
+    appVersion: '1766832802730'
 };
