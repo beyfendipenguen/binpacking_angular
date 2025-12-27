ec8f31e1865fcac8e2bdb94683b136504d768f12
hllyrtmn <halilyurtman@gmail.com>
2025-12-28 02:54:44 +0300

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     production: true,
     name: 'deploy',
     apiUrl: 'https://test-api.industricode.com/api',
-    appVersion: '1766848558047'
+    appVersion: '1766879578347'
 };
